2dac6140a08a963432f8928ced9af7cbc534cd67
Peter Coughlin <coughlin1966@gmail.com>
2018-02-13 18:18:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,11 @@
 def turn_count(board)
-total = 0
 counter = 0
-counter_x = 0
-counter_o = 0
 board.each do |index|
-  if index == " "
-    counter += 0
-  elsif
-    index == "X"
-    counter_x += 1
-  else
-    index == "O"
-    counter_o +=1
+  if index != " "
+    counter += 1
   end
 end
-total = counter_o + counter_x
+counter
 end
 
 def current_player(board)
